c8794171e31b5d8c8bdfbc91c82050e23be900dc
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2019-08-30 09:10:35 +0200

    Create QRC (Qt Resource File) to be compiled into Python file

--- App/easyDiffraction.py
+++ App/easyDiffraction.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+import QmlResource
+
 from PySide2.QtCore import QUrl, Qt, QCoreApplication
 from PySide2.QtWidgets import QApplication
 from PySide2.QtQml import QQmlApplicationEngine
@@ -20,8 +22,8 @@ if __name__ == '__main__':
     engine = QQmlApplicationEngine()
     engine.rootContext().setContextProperty("proxy", proxy)
 
-    engine.addImportPath(os.path.join(os.path.dirname(sys.argv[0]), "Imports"))
-    engine.load(QUrl.fromLocalFile(os.path.join(os.path.dirname(sys.argv[0]), "Gui.qml")))
+    engine.addImportPath(":/Imports")
+    engine.load(":/Gui.qml")
 
     if not engine.rootObjects():
         sys.exit(-1)
---
+++ Cpp.pro
@@ -0,0 +1,1 @@
+RESOURCES += App/QmlResource.qrc
