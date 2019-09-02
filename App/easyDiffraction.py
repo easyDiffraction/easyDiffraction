66f6fcc50db43ea7bac9d735b15e031f7a695679
Piotr Rozyczko <piotr.rozyczko@gmail.com>
2019-09-02 11:39:38 +0200

    It seems __init__.py in App was the culprit. Removed it.

--- App/easyDiffraction.py
+++ App/easyDiffraction.py
@@ -7,25 +7,42 @@ from PySide2.QtCore import QUrl, Qt, QCoreApplication
 from PySide2.QtWidgets import QApplication
 from PySide2.QtQml import QQmlApplicationEngine
 
+import QmlResource
+
 import RhoChiQml
 
+class MainWindow():
+    def __init__(self):
+        QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+
+        self.setupApp()
+        proxy = RhoChiQml.Proxy()
+        self.setupEngine(proxy=proxy)
+
+    def setupApp(self):
+        """
+        Inital setup of the main application window
+        """
+        self.app = QApplication(sys.argv)
+        self.app.setOrganizationName("easyDiffraction")
+        self.app.setOrganizationDomain("easyDiffraction.org")
+        self.app.setApplicationName("easyDiffraction")
+
+    def setupEngine(self, proxy=None):
+        """
+        Initial setup of the QmlApplication Engine
+        """
+        self.engine = QQmlApplicationEngine()
+        self.engine.rootContext().setContextProperty("proxy", proxy)
+
+        self.engine.addImportPath(":/Imports")
+        self.engine.load(":/Gui.qml")
+
+    def exec(self):
+        self.app.exec_()
+
 if __name__ == '__main__':
-    QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
 
-    app = QApplication(sys.argv)
-    app.setOrganizationName("easyDiffraction")
-    app.setOrganizationDomain("easyDiffraction.org")
-    app.setApplicationName("easyDiffraction")
-
-    proxy = RhoChiQml.Proxy()
-
-    engine = QQmlApplicationEngine()
-    engine.rootContext().setContextProperty("proxy", proxy)
-
-    engine.addImportPath(":/Imports")
-    engine.load(":/Gui.qml")
-
-    if not engine.rootObjects():
-        sys.exit(-1)
-
-    sys.exit(app.exec_())
+    main_app = MainWindow()
+    if main_app.engine.rootObjects():
+        sys.exit(main_app.exec())
