478f94400762904a4ba10ebcbb50e028ad2e7e4a
Piotr Rozyczko <piotr.rozyczko@gmail.com>
2019-09-03 11:25:08 +0200

    Try reverting a few things

--- App/easyDiffraction.py
+++ App/easyDiffraction.py
@@ -1,8 +1,6 @@
 import os
 import sys
 
-import QmlResource
-
 from PySide2.QtCore import QUrl, Qt, QCoreApplication
 from PySide2.QtWidgets import QApplication
 from PySide2.QtQml import QQmlApplicationEngine
@@ -23,7 +21,11 @@ if __name__ == '__main__':
     engine = QQmlApplicationEngine()
     engine.rootContext().setContextProperty("proxy", proxy)
 
-    engine.addImportPath(":/Imports")
-    engine.load(":/Gui.qml")
+    #engine.addImportPath(":/Imports")
+    #engine.load(":/Gui.qml")
+
+    engine.addImportPath(os.path.join(os.path.dirname(sys.argv[0]), "Imports"))
+    engine.load(QUrl.fromLocalFile(os.path.join(os.path.dirname(sys.argv[0]), "Gui.qml")))
+
     if engine.rootObjects():
         sys.exit(app.exec_())
