7edff096e650e938d35bb5bebdaf6095ea688421
Piotr Rozyczko <piotr.rozyczko@gmail.com>
2019-09-04 15:41:05 +0200

    Added squish tests for file load and corresponding behaviour in tab1 and tab2

--- Tests/squish_tests/run_test.bat
+++ Tests/squish_tests/run_test.bat
@@ -1,13 +1,15 @@
 set SQUISHPATH=c:\util\Squish651_Qt513_vs2017
 set SQUISH_SERVER_HOST=127.0.0.1
 set SQUISH_SERVER_PORT=4322
-set TESTSUITE=C:\temp\suite_easyDiff_tab1
+set TESTSUITE_1=C:\temp\suite_easyDiff_tab1
+set TESTSUITE_2=C:\temp\suite_easyDiff_load
 set REPORTPATH=c:\temp\report
 set PYTHONPATH=C:\Anaconda\env\p27
 
 cd %SQUISHPATH%\bin
 
-squishrunner --host %SQUISH_SERVER_HOST% --port %SQUISH_SERVER_PORT% --testsuite %TESTSUITE% --reportgen xml2.1,%REPORTPATH%/report.xml
+squishrunner --host %SQUISH_SERVER_HOST% --port %SQUISH_SERVER_PORT% --testsuite %TESTSUITE_1% --reportgen xml2.1,%REPORTPATH%/report.xml
+squishrunner --host %SQUISH_SERVER_HOST% --port %SQUISH_SERVER_PORT% --testsuite %TESTSUITE_2% --reportgen xml2.1,%REPORTPATH%/report.xml
 
 cd %REPORTPATH%
 
---
+++ Tests/squish_tests/suite_easyDiff_load/shared/scripts/names.py
@@ -0,0 +1,31 @@
+# encoding: UTF-8
+
+from objectmaphelper import *
+
+easyDiffraction_QQuickApplicationWindow = {"title": "easyDiffraction", "type": "QQuickApplicationWindow", "unnamed": 1, "visible": True}
+easyDiffraction_Overlay = {"container": easyDiffraction_QQuickApplicationWindow, "type": "Overlay", "unnamed": 1, "visible": True}
+label_MnemonicLabel = {"container": easyDiffraction_Overlay, "objectName": "label", "occurrence": 2, "type": "MnemonicLabel", "visible": True}
+easyDiffraction_No_Project_Created_Opened_Text = {"container": easyDiffraction_QQuickApplicationWindow, "text": "No Project Created/Opened", "type": "Text", "unnamed": 1, "visible": True}
+easyDiffraction_Text = {"container": easyDiffraction_QQuickApplicationWindow, "occurrence": 2, "type": "Text", "unnamed": 1, "visible": True}
+label_MnemonicLabel_2 = {"container": easyDiffraction_Overlay, "objectName": "label", "occurrence": 3, "type": "MnemonicLabel", "visible": True}
+o_Rectangle = {"checkable": False, "container": easyDiffraction_QQuickApplicationWindow, "id": "titleArea", "text": "Program Preferences", "type": "Button", "unnamed": 1, "visible": True}
+analysis_Button = {"checkable": False, "container": easyDiffraction_QQuickApplicationWindow, "id": "titleArea", "text": "Program Preferences", "type": "Button", "unnamed": 1, "visible": True}
+easyDiffraction_image_IconImage = {"container": easyDiffraction_QQuickApplicationWindow, "objectName": "image", "occurrence": 3, "source": "file::/Imports/easyAnalysis/Resources/Icons/triangle-right.svg", "type": "IconImage", "visible": True}
+easyDiffraction_Show_Animated_Intro_CheckBox = {"checkable": True, "container": easyDiffraction_QQuickApplicationWindow, "text": "Show Animated Intro", "type": "CheckBox", "unnamed": 1, "visible": True}
+easyDiffraction_Show_User_Guides_CheckBox = {"checkable": True, "container": easyDiffraction_QQuickApplicationWindow, "text": "Show User Guides", "type": "CheckBox", "unnamed": 1, "visible": True}
+easyDiffraction_Examples_Button = {"checkable": False, "container": easyDiffraction_QQuickApplicationWindow, "id": "titleArea", "text": "Examples", "type": "Button", "unnamed": 1, "visible": True}
+easyDiffraction_Recent_Projects_Button = {"checkable": False, "container": easyDiffraction_QQuickApplicationWindow, "id": "titleArea", "text": "Recent Projects", "type": "Button", "unnamed": 1, "visible": True}
+easyDiffraction_Experimental_Data_Button = {"checkable": False, "container": easyDiffraction_QQuickApplicationWindow, "text": "Experimental Data", "type": "Button", "unnamed": 1, "visible": True}
+analysis_Button_2 = {"checkable": True, "container": easyDiffraction_Overlay, "id": "analysisButton", "text": "Analysis", "type": "Button", "unnamed": 1, "visible": True}
+easyDiffraction_Open_another_project_Button = {"checkable": False, "container": easyDiffraction_QQuickApplicationWindow, "text": "Open another project", "type": "Button", "unnamed": 1, "visible": True}
+easyDiffraction_Fe3O4_Text = {"container": easyDiffraction_QQuickApplicationWindow, "text": "Fe3O4", "type": "Text", "unnamed": 1, "visible": True}
+easyDiffraction_Image = {"container": easyDiffraction_QQuickApplicationWindow, "source": "file:///C:/cygwin/home/piotrrozyczko/projects/easyDiffraction/Examples/Fe3O4_0T_powder_1d/structure.png", "type": "Image", "unnamed": 1, "visible": True}
+easyDiffraction_Text_2 = {"container": easyDiffraction_QQuickApplicationWindow, "occurrence": 16, "type": "Text", "unnamed": 1, "visible": True}
+easyDiffraction_tabBar_TabBar = {"container": easyDiffraction_QQuickApplicationWindow, "id": "tabBar", "occurrence": 2, "type": "TabBar", "unnamed": 1, "visible": True}
+tabBar_tabBar_TabBar = {"container": easyDiffraction_tabBar_TabBar, "id": "tabBar", "type": "TabBar", "unnamed": 1, "visible": True}
+tabBar_Table_View_TabButton = {"checkable": True, "container": tabBar_tabBar_TabBar, "text": "Table View", "type": "TabButton", "unnamed": 1, "visible": True}
+easyDiffraction_tabBar_TabBar_2 = {"container": easyDiffraction_QQuickApplicationWindow, "id": "tabBar", "type": "TabBar", "unnamed": 1, "visible": True}
+tabBar_Experimental_Data_Button = {"checkable": True, "container": easyDiffraction_tabBar_TabBar_2, "text": "Experimental Data", "type": "Button", "unnamed": 1, "visible": True}
+tabBar_label_MnemonicLabel = {"container": tabBar_tabBar_TabBar, "objectName": "label", "occurrence": 3, "type": "MnemonicLabel", "visible": True}
+easyDiffraction_ScrollView = {"container": easyDiffraction_QQuickApplicationWindow, "type": "ScrollView", "unnamed": 1, "visible": True}
+textArea_TextArea = {"container": easyDiffraction_ScrollView, "id": "textArea", "type": "TextArea", "unnamed": 1, "visible": True}
