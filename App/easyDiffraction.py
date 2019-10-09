d858e38a7d42cf65a7e47ff34675476ecd0c6423
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2019-10-09 12:49:03 +0200

    Add Examples folder to default path when lunch app 1st time

--- App/easyDiffraction.py
+++ App/easyDiffraction.py
@@ -12,14 +12,24 @@ from PyImports.Proxy import Proxy
 
 current_dir = os.path.dirname(sys.argv[0])
 
+def installationPath():
+    if sys.platform.startswith('win'):
+        return os.path.realpath(os.path.join(current_dir, '..'))
+    elif sys.platform.startswith('darwin'):
+        return os.path.realpath(os.path.join(current_dir, '..', '..', '..'))
+    return os.path.join(current_dir)
+
+def logFilePath(log_name):
+    return os.path.join(installationPath(), log_name)
+
 logging.basicConfig(
     format = "%(asctime)-15s [%(levelname)s] %(filename)s %(funcName)s [%(lineno)d]: %(message)s",
     level = logging.INFO,
-    #filename = os.path.join(current_dir, 'easyDiffraction.log'),
-    #filemode = 'w'
+    filename = logFilePath('easyDiffraction.log'),
+    filemode = 'w'
     )
 logger = logging.getLogger()
-logger.disabled = True
+#logger.disabled = True
 
 if __name__ == '__main__':
     QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
@@ -32,11 +42,13 @@ if __name__ == '__main__':
 
     proxy = Proxy()
 
+    examples_dir = os.path.join(installationPath(), 'Examples')
     qml_gui_path = os.path.join(current_dir, "Gui.qml")
     imports_path = os.path.join(current_dir, "QmlImports")
 
     engine = QQmlApplicationEngine()
     engine.rootContext().setContextProperty("proxy", proxy)
+    engine.rootContext().setContextProperty("examplesDir", examples_dir)
     engine.addImportPath(imports_path)
     engine.load(qml_gui_path)
     #engine.addImportPath(":/QmlImports")
