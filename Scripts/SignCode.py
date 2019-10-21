5376de71f366ed80614b9130dd8137ff22da3a72
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2019-10-21 15:17:55 +0200

    Try to fix codesign

--- Scripts/SignCode.py
+++ Scripts/SignCode.py
@@ -18,6 +18,10 @@ print('os_name:', os_name)
 # Passwords
 certificate_password_dict = ast.literal_eval(sys.argv[2]) if len(sys.argv) > 2 else {'osx': '', 'windows': ''}
 certificate_password = certificate_password_dict[os_name]
+print("sys.argv[3]", sys.argv[3])
+s = str(sys.argv[3])
+print("s", s)
+print("type(s)", type(s))
 zip_password = sys.argv[3] if len(sys.argv) > 3 else ''
 print(zip_password)
 
