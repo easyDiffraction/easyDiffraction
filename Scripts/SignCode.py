698d3a0e8fac074681872607f4a508cd74c60b3d
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2019-10-21 20:04:56 +0200

    Exit sigcode.py if linux

--- Scripts/SignCode.py
+++ Scripts/SignCode.py
@@ -14,9 +14,10 @@ print('\n***** Variables')
 # Os
 os_name = sys.argv[1] if len(sys.argv) > 1 else 'osx'
 print('os_name:', os_name)
+if os_name == 'linux': exit()
 
 # Passwords
-passwords_dict = ast.literal_eval(sys.argv[2]) if len(sys.argv) > 2 else {'osx':'', 'windows':'', 'linux':'', 'zip':''}
+passwords_dict = ast.literal_eval(sys.argv[2]) if len(sys.argv) > 2 else {'osx':'', 'windows':'', 'zip':''}
 certificate_password = passwords_dict[os_name].replace('\\', '')
 zip_password = passwords_dict['zip']
 
@@ -142,6 +143,3 @@ elif (os_name == 'windows'):
             ]
     result = subprocess.run(args, stdout=subprocess.PIPE).stdout.decode('utf-8')
     print(result)
-
-# End
-print()
