3c3ef278b07cf3ce8cd1e1d4bac80715f2aaf312
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2019-11-19 13:06:05 +0100

    Create project config yml file

--- Scripts/UploadArtifacts.py
+++ Scripts/UploadArtifacts.py
@@ -7,144 +7,111 @@ import subprocess
 import zipfile
 import requests
 from uritemplate import URITemplate
+import yaml     # pip3 install pyyaml
 import Variables
 
-################
-# GET PARAMETERS
-################
-
-# Global constants
-var = Variables.VarsConfig()
-
-# BRANCH NAME
-branch_name = os.environ['TRAVIS_BRANCH']
-if branch_name != '':
-    print("Branch name: '{0}'".format(branch_name))
-else:
-    print("Failed to get branch name.")
-    exit()
-
-# RELEASE INFO
-
-# Project
-project_name = 'easyDiffraction'
-
-# Main paths
-scripts_dir_path = os.path.dirname(os.path.realpath(__file__))
-project_dir_path = os.path.realpath(os.path.join(scripts_dir_path, '..'))
-
-# Settings file
-settings_file_path = os.path.join(project_dir_path, 'App', 'QmlImports', 'easyDiffraction', 'Settings.qml')
-
-# Find application version
-release_version = ''
-with open(settings_file_path, 'r') as f:
-    file_content = f.read()
-    release_version = re.findall('\d+.\d+.\d+', file_content)[0]
-    print("Release version: '{0}'".format(release_version))
-
-# RELEASE TAG
-release_tag = ''
-if re.search('v\d+\.\d+\.\d+', branch_name): # if release with tag of 'v1.0.12' format
-    release_tag = 'v{0}'.format(release_version)
-else:
-    release_tag = '{0}-v{1}'.format(branch_name, release_version)
-print("Release tag: '{0}'".format(release_tag))
-
-# RELEASE NAME
-release_name = 'easydiffraction-' + release_tag
-print("Release name: '{0}'".format(release_name))
-
-# ACCESS TOKEN
-access_token = os.environ['GITHUB_TOKEN']
-
-# FILE TO UPLOAD
-upload_file_name = '{0}_{1}_v{2}.zip'.format(var.project_name, var.os_name, release_version)
-upload_file_path = os.path.join(var.installer_dir_path, upload_file_name)
-print("Upload file name: '{0}'".format(upload_file_name))
-print("Upload file path: '{0}'".format(upload_file_path))
-
-################
-# CREATE RELEASE
-################
-
-# if it doesn't exist yet
-#https://developer.github.com/v3/repos/releases/#create-a-release
-#https://stackoverflow.com/questions/5207269/how-to-release-a-build-artifact-asset-on-github-with-a-script
-
-##################
-# UPLOAD ARTIFACTS
-##################
-
-owner = 'easydiffraction'
-repo = 'easydiffraction'
-authorization_info = {'Authorization': 'Token {0}'.format(access_token)}
-api_base_url = 'https://api.github.com/repos/{0}/{1}'.format(owner, repo)
-content_type_info = {'Content-Type': 'application/zip'}
+# Read config
+config_file_path = os.path.join(os.getcwd(), 'Configs', 'Project.yml')
+with open(config_file_path, 'r') as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
 
 # Get list of all the releases (including draft ones)
-releases_url = '{0}/releases'.format(api_base_url)
-response = requests.get(releases_url, headers=authorization_info)
+response = requests.get(config['github']['releases_url'], headers=config['github']['auth_header'])
 releases_list = response.json()
 if response:
-    print("Succeeded to get list of all releases for '{0}'".format(api_base_url))
+    print("Succeeded to get list of all releases for '{0}'".format(config['github']['api_base_url']))
 else:
-    print("Failed to get list of all releases for '{0}'".format(api_base_url))
+    print("Failed to get list of all releases for '{0}'".format(config['github']['api_base_url']))
     exit()
 
-# Get both id and upload_url of the specific release
+# Get id of the specific release, if release is already exist
+release_exist = False
+for release in releases_list:
+    if release['tag_name'] == config['ci']['branch']:
+        release_exist = True
+        break
+
+# Create release, if it doesn't exist yet
+#https://developer.github.com/v3/repos/releases/#create-a-release
+#https://stackoverflow.com/questions/5207269/how-to-release-a-build-artifact-asset-on-github-with-a-script
+if release_exist:
+    print("Release '{0}' already exists".format(config['ci']['branch'])) # release_tag
+else:
+    print("Release '{0}' doesn't exist, creating".format(config['ci']['branch'])) # release_tag
+    release_description = {
+      "tag_name": config['release']['tag'],
+      "target_commitish": config['ci']['branch'],
+      "name": config['release']['name'],
+      "draft": config['release']['draft'],
+      "prerelease": config['release']['prerelease']
+    }
+    response = requests.post(config['github']['releases_url'], headers=config['github']['upload_zip_header'], json=release_description)
+    if response:
+        print("Release '{0}' is successfully created".format(release_description['name']))
+    else:
+        print("Failed to create release '{0}'".format(release_description['name']))
+        #print(response.status_code)
+        #print(response.text)
+        exit()
+    # Reread list list of all the releases (including draft ones)
+    response = requests.get(config['github']['releases_url'], headers=config['github']['auth_header'])
+    releases_list = response.json()
+    if response:
+        print("Succeeded to get list of all releases for '{0}'".format(config['github']['api_base_url']))
+    else:
+        print("Failed to get list of all releases for '{0}'".format(config['github']['api_base_url']))
+        exit()
+
+# Get id and upload_url of the specific release, if release is already exist
 release_id = ''
 release_upload_url = ''
 for release in releases_list:
-    if release['tag_name'] == release_tag:
+    if release['tag_name'] == config['ci']['branch']:
         release_id = release['id']
         release_upload_url = release['upload_url']
-        print("Succeeded to get release_id and release_upload_url.")
-        ###print("release_id: {0}".format(release_id))
-        ###print("release_upload_url: {0}".format(release_upload_url))
         break
-    else:
-        print("Failed to get release_id and release_upload_url.")
-        exit()
 
 # Get list of assets for the specific release
-release_assets_url = '{0}/releases/{1}/assets'.format(api_base_url, release_id)
-response=requests.get(release_assets_url, headers=authorization_info)
+release_assets_url = '{0}/releases/{1}/assets'.format(config['github']['api_base_url'], release_id)
+print('release_assets_url', release_assets_url)
+response=requests.get(release_assets_url, headers=config['github']['auth_header'])
 release_assets_dict = response.json()
 if response:
     print("Succeeded to get list of assets")
 else:
     print("Failed to get list of assets for release")
+    print(response.status_code)
+    print(response.text)
     exit()
 
 # Delete existing asset (if any) and show info message
 for asset in release_assets_dict:
-    if asset['name'] == upload_file_name:
-        print("File '{0}' already exists, deleting.".format(upload_file_name))
-        asset_url = '{0}/releases/assets/{1}'.format(api_base_url, asset['id'])
-        response = requests.delete(asset_url, headers=authorization_info)
+    if asset['name'] == config['release']['file_name']:
+        print("File '{0}' already exists, deleting.".format(config['release']['file_name']))
+        asset_url = '{0}/releases/assets/{1}'.format(config['github']['api_base_url'], asset['id'])
+        response = requests.delete(asset_url, headers=config['github']['auth_header'])
         if response:
-            print("Succeeded to delete asset '{0}'".format(upload_file_name))
+            print("Succeeded to delete asset '{0}'".format(config['release']['file_name']))
         else:
-            print("Failed to delete asset '{0}'".format(upload_file_name))
+            print("Failed to delete asset '{0}'".format(config['release']['file_name']))
             exit()
 
 # Upload asset and show info message
-asset_upload_url = URITemplate(release_upload_url).expand(name=upload_file_name) # rename file to be uploaded, if needed
-#print("asset_upload_url '{0}'".format(asset_upload_url))
-
-response = requests.post(asset_upload_url, headers={**authorization_info, **content_type_info}, data=open(upload_file_path, 'rb').read()) # use local file
+asset_upload_url = URITemplate(release_upload_url).expand(name=config['release']['file_name']) # rename file to be uploaded, if needed
+response = requests.post(asset_upload_url, headers=config['github']['upload_zip_header'], data=open(config['release']['file_path'], 'rb').read()) # use local file
 if response:
-    print("File '{0}' is successfully uploaded.".format(upload_file_name))
+    print("File '{0}' is successfully uploaded.".format(config['release']['file_name']))
 else:
-    print("Failed to upload '{0}' file.".format(upload_file_name))
+    print("Failed to upload '{0}' file.".format(config['release']['file_name']))
     exit()
+
+
+
 
 # Check
 #https://github.com/smarie/python-mini-lambda/blob/master/ci_tools/github_release.py
 #https://docs.travis-ci.com/user/deployment/pages/#setting-the-github-token
 #https://developer.github.com/v3/repos/
-
 
 # Pre-commit: Referencing current branch in github readme.md
 #https://stackoverflow.com/questions/18673694/referencing-current-branch-in-github-readme-md/35368808#35368808
