2a998fdc714730e00c4e0accc0c9e6f53b7d2263
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2019-11-21 17:52:19 +0100

    Update Project.py

--- Scripts/Project.py
+++ Scripts/Project.py
@@ -9,10 +9,10 @@ import PySide2, shiboken2 # pip install pyside2
 # FUNCTIONS
 
 def printTitle(title):
-    print('{0}***** {1}'.format(os.linesep, title))
+    #print('{0}***** {1}'.format(os.linesep, title))
+    print('***** {0}'.format(title))
 
-def run(title, args):
-    printTitle(title)
+def run(*args):
     result = subprocess.run(
         args,
         #capture_output=True,
@@ -33,12 +33,42 @@ class Config():
     def __init__(self):
         self._config_dir = os.getcwd()
         self._config_name = 'Project.yml'
+        # load external config
         self.__dict__ = self._loadYaml(self._config_dir, self._config_name)
-        self.__dict__['project']['dir_path'] = os.getcwd()
+        # project
+        self.__dict__['project']['dir_path'] = os.getcwd() # ??? self._config_dir
+        self.__dict__['project']['subdirs']['distribution']['path'] = self._absolutePath(self.__dict__['project']['subdirs']['distribution']['name'])
+        self.__dict__['project']['subdirs']['scripts']['path'] = self._absolutePath(self.__dict__['project']['subdirs']['scripts']['name'])
+        self.__dict__['project']['subdirs']['certificates']['path'] = self._absolutePath(self.__dict__['project']['subdirs']['certificates']['name'])
+        self.__dict__['project']['subdirs']['examples']['path'] = self._absolutePath(self.__dict__['project']['subdirs']['examples']['name'])
         self.__dict__['os']['name'] = self._osName()
+        # scripts
+        self.__dict__['scripts']['silent_install'] = os.path.join(self.__dict__['project']['subdirs']['scripts']['path'], 'SilentInstall.js')
+        # application
+        self.__dict__['app']['freezed_path'] = os.path.join(self.__dict__['project']['subdirs']['distribution']['path'], self.__dict__['app']['name'] + self.__dict__['app']['freezed_ext'][self._osName()])
+        # user
+        self.__dict__['user']['home_dir'] = os.path.expanduser('~')
+        # freeze app
         self.__dict__['pyinstaller']['lib_path']['cryspy'] = cryspy.__path__[0]
         self.__dict__['pyinstaller']['lib_path']['shiboken2'] = shiboken2.__path__[0]
         self.__dict__['pyinstaller']['lib_path']['pyside2'] = PySide2.__path__[0]
+        # installer framework
+        self.__dict__['qtifw']['setup']['name'] = '{0}.{1}'.format(self.__dict__['qtifw']['setup']['base'][self._osName()], self.__dict__['qtifw']['setup']['ext'][self._osName()])
+        self.__dict__['qtifw']['setup']['download_url'] = 'https://download.qt.io/official_releases/qt-installer-framework/{0}/{1}'.format(self.__dict__['qtifw']['version'], self.__dict__['qtifw']['setup']['name'])
+        self.__dict__['qtifw']['setup']['download_path'] = os.path.join(self.__dict__['project']['subdirs']['distribution']['path'], self.__dict__['qtifw']['setup']['name'])
+        self.__dict__['qtifw']['setup']['exe_path'] = self._qtifwExe(self._osName())
+        self.__dict__['qtifw']['bin_dir_path'] = self._binDirPath(self._osName())
+        self.__dict__['qtifw']['binarycreator_path'] = os.path.join(self.__dict__['qtifw']['bin_dir_path'], 'binarycreator')
+        self.__dict__['qtifw']['installerbase_path'] = os.path.join(self.__dict__['qtifw']['bin_dir_path'], 'installerbase')
+        # app installer
+        self.__dict__['app']['installer']['config_dir_path'] = os.path.join(self.__dict__['project']['dir_path'], 'Installer')
+        self.__dict__['app']['installer']['config_xml_path'] = os.path.join(self.__dict__['app']['installer']['config_dir_path'], 'config', 'config.xml')
+        self.__dict__['app']['installer']['packages_dir_path'] = os.path.join(self.__dict__['app']['installer']['config_dir_path'], 'packages')
+        self.__dict__['app']['installer']['data_dir_path'] = os.path.join(self.__dict__['app']['installer']['packages_dir_path'], 'io.github.easydiffraction', 'data')
+        self.__dict__['app']['installer']['dir_path'] = self.__dict__['project']['subdirs']['distribution']['path']
+        self.__dict__['app']['installer']['name'] = self.__dict__['app']['name'] + 'Installer'
+        self.__dict__['app']['installer']['exe_name'] = self.__dict__['app']['installer']['name'] + self.__dict__['os']['gui_exe_ext'][self._osName()]
+        self.__dict__['app']['installer']['exe_path'] = os.path.join(self.__dict__['app']['installer']['dir_path'], self.__dict__['app']['installer']['exe_name'])
 
     def __getitem__(self, key):
         return self.__dict__[key]
@@ -54,6 +84,10 @@ class Config():
             file_content = yaml.load(file, Loader=yaml.FullLoader)
             return file_content
 
+    def _absolutePath(self, relative_path):
+        project_dir_path = self.__dict__['project']['dir_path']
+        return os.path.join(project_dir_path, relative_path)
+
     def _osName(self):
         platform = sys.platform
         if platform.startswith('darwin'):
@@ -65,6 +99,22 @@ class Config():
         else:
             print("* Unsupported platform '{0}'".format(platform))
             return None
+
+    def _qtifwExe(self, os_name):
+        d = {
+            'osx': '/Volumes/{0}/{0}.app/Contents/MacOS/{0}'.format(self.__dict__['qtifw']['setup']['base'][os_name]),
+            'windows': self.__dict__['qtifw']['setup']['download_path'],
+            'linux': self.__dict__['qtifw']['setup']['download_path']
+        }
+        return d[os_name]
+
+    def _binDirPath(self, os_name):
+        d = {
+            'osx': '{0}/Qt/QtIFW-{1}/bin'.format(self.__dict__['user']['home_dir'], self.__dict__['qtifw']['version']),
+            'windows': 'C:\\Qt\\QtIFW-{0}\\bin'.format(self.__dict__['qtifw']['version']),
+            'linux': '{0}/Qt/QtIFW-{1}/bin'.format(self.__dict__['user']['home_dir'], self.__dict__['qtifw']['version'])
+        }
+        return d[os_name]
 
 # MAIN
 
