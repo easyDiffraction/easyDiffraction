b924922bf08d0faf68cb35abec3ee12bbfd47a15
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2019-11-22 08:23:10 +0100

    Keep only stderr (no stdout) in scripts

--- Scripts/Project.py
+++ Scripts/Project.py
@@ -21,8 +21,8 @@ def run(*args):
         universal_newlines=True,    # converts the output to a string instead of a byte array.
         #check=True                  # forces the Python method to throw an exception if the underlying process encounters errors
     )
-    if result.stdout:
-        print("+ Success:{0}{1}".format(os.linesep, result.stdout))
+    #if result.stdout:
+    #    print("+ Success:{0}{1}".format(os.linesep, result.stdout))
     if result.stderr:
         print("- Fail:{0}{1}".format(os.linesep, result.stderr))
         sys.exit()
