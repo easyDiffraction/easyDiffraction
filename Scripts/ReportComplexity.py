d2bc4960e51b06a4deed6fe3fcbb1ebada5405d0
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2019-12-02 16:33:25 +0100

    Update ReportComplexity.py

--- Scripts/ReportComplexity.py
+++ Scripts/ReportComplexity.py
@@ -20,12 +20,14 @@ if __name__ == "__main__":
     config = Project.Config()
     app_dir_path = config['project']['subdirs']['app']['path']
     file_paths = pyFilePaths(app_dir_path)
+    print(file_paths)
 
     Functions.run(
         'wily',
         'build',                                                            # By default, it will assume the directory is a git repository and will scan back through 50 revisions
         app_dir_path,                                                       # directory to be scanned
-        '--max-revisions', '2'                                              # -n, --max-revisions <max_revisions>: The maximum number of historical commits to archive
+        '--max-revisions', '2',                                             # -n, --max-revisions <max_revisions>: The maximum number of historical commits to archive
+        report_success=True
         )
 
     Functions.run(
@@ -35,4 +37,5 @@ if __name__ == "__main__":
         '--changes-only',                                                   # --all, --changes-only: Show all files, instead of changes only
         '--no-detail',                                                      # --detail, --no-detail: Show function/class level metrics where available
         '--metrics', 'maintainability.mi,cyclomatic.complexity,raw.loc',    # --metrics <metrics>: comma-seperated list of metrics, see list-metrics for choices
+        report_success=True
         )
