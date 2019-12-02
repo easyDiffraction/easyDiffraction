aacc81f491612dcb78c02c382f2fcfdf1822e1a1
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2019-12-02 18:16:01 +0100

    Do not run report complexity on Travis

--- Scripts/ReportComplexity.py
+++ Scripts/ReportComplexity.py
@@ -31,19 +31,22 @@ if __name__ == "__main__":
     project_dir_path = config['project']['dir_path']
     app_dir_path = config['project']['subdirs']['app']['path']
     file_paths = pyFilePaths(app_dir_path)
-    #branch = environmentVariable('TRAVIS_BRANCH')
 
-    Functions.runAsIs(
-        'wily',
-        'build',                                                            # By default, it will assume the directory is a git repository and will scan back through 50 revisions
-        app_dir_path,                                                       # directory to be scanned
-        '--max-revisions', '1',                                             # -n, --max-revisions <max_revisions>: The maximum number of historical commits to archive
-        )
-    Functions.runAsIs(
-        'wily',
-        'diff',                                                             # compare the last cached revision of the code with the current metrics (for the current environment).
-        *file_paths,                                                        # to see the changes in metrics for a list of files
-        '--changes-only',                                                   # --all, --changes-only: Show all files, instead of changes only
-        '--no-detail',                                                      # --detail, --no-detail: Show function/class level metrics where available
-        '--metrics', 'maintainability.mi,cyclomatic.complexity,raw.loc',    # --metrics <metrics>: comma-seperated list of metrics, see list-metrics for choices
-        )
+    if environmentVariable('TRAVIS_BRANCH'):
+        print('Not implemented yet')
+
+    else:
+        Functions.runAsIs(
+            'wily',
+            'build',                                                            # By default, it will assume the directory is a git repository and will scan back through 50 revisions
+            app_dir_path,                                                       # directory to be scanned
+            '--max-revisions', '1',                                             # -n, --max-revisions <max_revisions>: The maximum number of historical commits to archive
+            )
+        Functions.runAsIs(
+            'wily',
+            'diff',                                                             # compare the last cached revision of the code with the current metrics (for the current environment).
+            *file_paths,                                                        # to see the changes in metrics for a list of files
+            '--changes-only',                                                   # --all, --changes-only: Show all files, instead of changes only
+            '--no-detail',                                                      # --detail, --no-detail: Show function/class level metrics where available
+            '--metrics', 'maintainability.mi,cyclomatic.complexity,raw.loc',    # --metrics <metrics>: comma-seperated list of metrics, see list-metrics for choices
+            )
