c456fd6e3ab4686745398bc904aba0bb95c3f8db
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2019-12-13 23:47:27 +0100

    Add message if coppy missing libs is not needed

--- Scripts/CopyMissingLibs.py
+++ Scripts/CopyMissingLibs.py
@@ -6,14 +6,19 @@ import Project
 import BasicFunctions
 
 if __name__ == "__main__":
-    config = Project.Config()
+    BasicFunctions.printTitle('Copy missing libraries')
 
+    config = Project.Config()
     os_name = config['os']['name']
     missing_files = config['pyinstaller']['missing_files'][os_name]
     shiboken2_path = config['pyinstaller']['lib_path']['shiboken2']
     pyside2_path = config['pyinstaller']['lib_path']['pyside2']
 
-    BasicFunctions.printTitle('Copy missing libraries')
+    if len(missing_files) == 0:
+        message = "* No missing libraries for os '{}'".format(os_name)
+        print(message)
+        exit()
+
     for file_name in missing_files:
         file_path = os.path.join(shiboken2_path, file_name)
         for file_path in glob.glob(file_path): # for cases with '*' in the lib name
