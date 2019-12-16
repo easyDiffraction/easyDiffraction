7c7f744dda992a1f34c7956bf78a8beb9c677264
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2019-12-16 08:35:15 +0100

    Set os dependent caches

--- Scripts/DeployToAppStore.py
+++ Scripts/DeployToAppStore.py
@@ -57,40 +57,40 @@ def moveCertificates(dir_name):
     else:
         BasicFunctions.printSuccessMessage(message)
 
-def saveDockerImage():
-    message = "save docker image '{}'".format('')
-    if os.path.exists('.soft/snap.tar.gz'):
-        message = "* Docker image for snapcraft is already downloaded {}".format('')
+def saveDockerImage(docker_image, downloaded_image_path):
+    message = "save docker image '{}'".format(docker_image)
+    if os.path.exists(downloaded_image_path):
+        message = "* Docker image '{0}' for snapcraft is already downloaded to '{1}'".format(docker_image, downloaded_image_path)
         print(message)
         return()
     try:
-        os.system('docker pull cibuilds/snapcraft:core18')
-        os.system('docker save cibuilds/snapcraft:core18 | gzip > .soft/snap.tar.gz')
+        os.system('docker pull {}'.format(docker_image))
+        os.system('docker save {0} | gzip > {1}'.format(docker_image, downloaded_image_path))
     except Exception as exception:
         BasicFunctions.printFailMessage(message, exception)
         sys.exit()
     else:
         BasicFunctions.printSuccessMessage(message)
 
-def loadDockerImage():
-    message = "load docker image '{}'".format('')
+def loadDockerImage(downloaded_image_path):
+    message = "load docker image from '{}'".format(downloaded_image_path)
     try:
         #os.system('docker load < .soft/snap.tar.gz')
-        os.system('gzip -dc .soft/snap.tar.gz | docker load')
+        os.system('gzip -dc {} | docker load'.format(downloaded_image_path))
     except Exception as exception:
         BasicFunctions.printFailMessage(message, exception)
         sys.exit()
     else:
         BasicFunctions.printSuccessMessage(message)
 
-def runDockerImage(project_dir_path, branch):
+def runDockerImage(docker_image, project_dir_path, branch):
     message = "push release 'edge' for branch '{}'".format(branch)
     try:
         BasicFunctions.run(
             'docker', 'run',
             '--volume', '{}:/easyDiffraction'.format(project_dir_path), # Bind mount a volume
             '--tty', # Allocate a pseudo-TTY
-            'cibuilds/snapcraft:core18',
+            docker_image,
             'sh', '-c', "apt update -qq && cd /easyDiffraction && snapcraft && snapcraft push *.snap --release edge"
             )
     except Exception as exception:
@@ -107,13 +107,15 @@ def osDependentDeploy():
     if os_name == 'linux':
         if branch == 'develop':
             snapcraft_dir_name = '.snapcraft'
+            docker_image = 'cibuilds/snapcraft:core18'
+            downloaded_image_path = '.soft/snap.tar.gz'
             createSnapcraftDir(snapcraft_dir_name)
             decryptCertificates()
             extractCertificates()
             moveCertificates(snapcraft_dir_name)
-            saveDockerImage()
-            loadDockerImage()
-            runDockerImage(project_dir_path, branch)
+            saveDockerImage(docker_image, downloaded_image_path)
+            loadDockerImage(downloaded_image_path)
+            runDockerImage(docker_image, project_dir_path, branch)
         else:
             message = "* No deployment for branch '{}' is implemented".format(branch)
             print(message)
