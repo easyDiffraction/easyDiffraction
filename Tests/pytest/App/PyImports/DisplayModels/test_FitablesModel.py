13ab15af93eec18eee118ca028e73be7bc6193a3
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2020-03-09 15:32:40 +0100

    Add some unit tests

--- Tests/pytest/App/PyImports/DisplayModels/test_FitablesModel.py
+++ Tests/pytest/App/PyImports/DisplayModels/test_FitablesModel.py
@@ -1,6 +1,6 @@
 import pytest
 
-from PySide2.QtCore import Qt, QUrl
+from PySide2.QtCore import Qt, QUrl, QModelIndex
 from PySide2.QtGui import QStandardItemModel
 
 from easyInterface.Diffraction.Calculators.CryspyCalculator import CryspyCalculator
@@ -10,7 +10,7 @@ import PyImports.DisplayModels.FitablesModel as Model
 
 TEST_FILE = "file:Tests/Data/main.cif"
 
-def test_FitablesModelModel():
+def no_test_FitablesModelModel():
 
     file_path = QUrl(TEST_FILE).toLocalFile()
     calculator = CryspyCalculator(file_path)
@@ -51,3 +51,146 @@ def test_FitablesModelModel():
 
     # test asModel
     assert m._model == m.asModel()
+
+
+def test_onModelChanged():
+    file_path = QUrl(TEST_FILE).toLocalFile()
+    calculator = CryspyCalculator(file_path)
+    interface = QtCalculatorInterface(calculator)
+
+    m = Model()
+    m.setCalculatorInterface(interface)
+    phase_index = m._model.index(2, 0)
+    experiment_index = m._model.index(m._model.rowCount()-1, 0)
+
+    # ######################
+    # Check refine parameter
+    # ######################
+
+    display_role = Qt.UserRole + 7
+    edit_role = Qt.UserRole + 107
+    old_display = False
+    old_edit = None
+    new_display = True
+    new_edit = True
+
+    # Initial state
+    assert m._model.data(phase_index, display_role) == old_display
+    assert m._model.data(phase_index, edit_role) == old_edit
+    assert m._model.data(experiment_index, display_role) == old_display
+    assert m._model.data(experiment_index, edit_role) == old_edit
+
+    # Model changes via display role
+    m._model.setData(phase_index, new_display, display_role)
+    assert m._model.data(phase_index, display_role) == new_display
+    assert m._model.data(phase_index, edit_role) == old_edit
+    m._model.setData(experiment_index, new_display, display_role)
+    assert m._model.data(experiment_index, display_role) == new_display
+    assert m._model.data(experiment_index, edit_role) == old_edit
+
+    # Model changes via edit role
+    m._model.setData(phase_index, new_edit, edit_role)
+    assert m._model.data(phase_index, display_role) == new_display
+    assert m._model.data(phase_index, edit_role) == new_edit
+    m._model.setData(experiment_index, new_edit, edit_role)
+    assert m._model.data(experiment_index, display_role) == new_display
+    assert m._model.data(experiment_index, edit_role) == new_edit
+
+    # #####################
+    # Check value parameter
+    # #####################
+
+    display_role = Qt.UserRole + 3
+    edit_role = Qt.UserRole + 103
+    old_display = 0
+    old_edit = None
+    new_display = 0.5
+    new_edit = 0.5
+
+    # Initial state
+    assert m._model.data(phase_index, display_role) == old_display
+    assert m._model.data(phase_index, edit_role) == old_edit
+    assert m._model.data(experiment_index, display_role) == old_display
+    assert m._model.data(experiment_index, edit_role) == old_edit
+
+    # Model changes via display role
+    m._model.setData(phase_index, new_display, display_role)
+    assert m._model.data(phase_index, display_role) == new_display
+    assert m._model.data(phase_index, edit_role) == old_edit
+    m._model.setData(experiment_index, new_display, display_role)
+    assert m._model.data(experiment_index, display_role) == new_display
+    assert m._model.data(experiment_index, edit_role) == old_edit
+
+    # Model changes via edit role
+    m._model.setData(phase_index, new_edit, edit_role)
+    assert m._model.data(phase_index, display_role) == new_display
+    assert m._model.data(phase_index, edit_role) == new_edit
+    m._model.setData(experiment_index, new_edit, edit_role)
+    assert m._model.data(experiment_index, display_role) == new_display
+    assert m._model.data(experiment_index, edit_role) == new_edit
+
+    # ###################
+    # Check min parameter
+    # ###################
+
+    display_role = Qt.UserRole + 5
+    edit_role = Qt.UserRole + 105
+    old_display = 0
+    old_edit = None
+    new_display = 0.5
+    new_edit = 0.5
+
+    # Initial state
+    assert m._model.data(phase_index, display_role) == old_display
+    assert m._model.data(phase_index, edit_role) == old_edit
+    assert m._model.data(experiment_index, display_role) == old_display
+    assert m._model.data(experiment_index, edit_role) == old_edit
+
+    # Model changes via display role
+    m._model.setData(phase_index, new_display, display_role)
+    assert m._model.data(phase_index, display_role) == new_display
+    assert m._model.data(phase_index, edit_role) == old_edit
+    m._model.setData(experiment_index, new_display, display_role)
+    assert m._model.data(experiment_index, display_role) == new_display
+    assert m._model.data(experiment_index, edit_role) == old_edit
+
+    # Model changes via edit role
+    m._model.setData(phase_index, new_edit, edit_role)
+    assert m._model.data(phase_index, display_role) == new_display
+    assert m._model.data(phase_index, edit_role) == new_edit
+    m._model.setData(experiment_index, new_edit, edit_role)
+    assert m._model.data(experiment_index, display_role) == new_display
+    assert m._model.data(experiment_index, edit_role) == new_edit
+
+    # ###################
+    # Check max parameter
+    # ###################
+
+    display_role = Qt.UserRole + 6
+    edit_role = Qt.UserRole + 106
+    old_display = 0
+    old_edit = None
+    new_display = 0.5
+    new_edit = 0.5
+
+    # Initial state
+    assert m._model.data(phase_index, display_role) == old_display
+    assert m._model.data(phase_index, edit_role) == old_edit
+    assert m._model.data(experiment_index, display_role) == old_display
+    assert m._model.data(experiment_index, edit_role) == old_edit
+
+    # Model changes via display role
+    m._model.setData(phase_index, new_display, display_role)
+    assert m._model.data(phase_index, display_role) == new_display
+    assert m._model.data(phase_index, edit_role) == old_edit
+    m._model.setData(experiment_index, new_display, display_role)
+    assert m._model.data(experiment_index, display_role) == new_display
+    assert m._model.data(experiment_index, edit_role) == old_edit
+
+    # Model changes via edit role
+    m._model.setData(phase_index, new_edit, edit_role)
+    assert m._model.data(phase_index, display_role) == new_display
+    assert m._model.data(phase_index, edit_role) == new_edit
+    m._model.setData(experiment_index, new_edit, edit_role)
+    assert m._model.data(experiment_index, display_role) == new_display
+    assert m._model.data(experiment_index, edit_role) == new_edit
