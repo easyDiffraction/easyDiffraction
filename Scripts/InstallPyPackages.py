47e2a3e4a9f5af021dd34b34be76b33f7ca36df6
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2020-04-07 00:20:03 +0200

    Use polarisation_start branch of easyInterface

--- Scripts/InstallPyPackages.py
+++ Scripts/InstallPyPackages.py
@@ -67,13 +67,13 @@ if __name__ == '__main__':
     upgradePip()
 
     installFromGit(owner='ikibalin', repo='cryspy', branch='transition-to-version-0.2')
-    #installFromGit(owner='easyDiffraction', repo='easyInterface', branch='master') # Until master branch is uploaded to PIP
+    installFromGit(owner='easyDiffraction', repo='easyInterface', branch='polarisation_start') # Until master branch is uploaded to PIP
 
     install(
         #'cryspy==0.1.13',
         'scipy>=1.4.1',
         'numpy>=1.18.1',
-        'easyInterface>=0.0.8',
+        #'easyInterface>=0.0.8',
         'PySide2>=5.14.1',
         'pyinstaller>=3.6',  # develop version - https://github.com/pyinstaller/pyinstaller/archive/develop.tar.gz
         'requests>=2.23.0',
