76096f4b06bfa3488a871a2670816533e2200902
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2020-05-06 12:25:34 +0200

    Add update from cif (for exps)

--- App/PyImports/ProxyPyQml.py
+++ App/PyImports/ProxyPyQml.py
@@ -122,7 +122,12 @@ class ProxyPyQml(QObject):
     @Slot(str)
     def updateExperimentFromGui(self, cif_string):
         exp_name = self._calculator_interface.experimentsIds()[0]
-        self._calculator_interface.updateExperiment(exp_name, cif_string)
+        cif_string = cif_string[cif_string.find('data_'):]
+        new_experiment = self._calculator_interface.getExperimentFromCif(cif_string)
+        old_exp = self._calculator_interface.getExperiment(exp_name)
+        keys, values = old_exp.dictComparison(new_experiment)
+        modded_keys = [['experiments', exp_name, *key] for key in keys]
+        self._calculator_interface.project_dict.bulkUpdate(modded_keys, values, 'Update Experiment from GUI')
         self.projectChanged.emit()
         self._need_to_save = False
         self.projectSaveStateChanged.emit()
