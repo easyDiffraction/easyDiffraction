ac6826766f30aa2be11148c1512c918603f5c32a
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2020-05-07 14:43:32 +0200

    Fix removing html tags from cif

--- App/QmlImports/easyDiffraction/Logic/Helpers.js
+++ App/QmlImports/easyDiffraction/Logic/Helpers.js
@@ -5,12 +5,15 @@ function highlightCifSyntax(str)
         .replace(/\n(_[\w,-]+)/ig, "\n<font color=#669431>$1</font>")   // colorize keys inside loops (with new line at the end), e.g., "_phase_label"
         .replace(/loop_/ig, "<font color=#888>loop_</font>")            // colorize "loop_"
         .replace(/data_\w+/ig, "<font color=#b95e39>$&</font>")         // colorize datablock, e.g., "data_Fe3O4"
-        .replace(/\n/ig, "<br />")                                      // change newline to html format
+        .replace(/(\r\n|\r|\n)/ig, "<br />")                            // change newline and carriage return escape sequences to html format
 }
 
 function removeHtmlTags(html)
 {
+    // regex tester: https://regexr.com/
     return html
-        .replace(/<br \/>/ig, "\n")     // convert newline from html format
-        .replace(/<\/?[^>]+>/ig, " ")   // remove html tags
+        .replace(/(\r\n|\r|\n)/ig, "")      // remove newline and carriage return escape sequences
+        .replace(/<br \/>/ig, "\n")         // convert newline from html format to escape sequences
+        .replace(/<style.+style>/ig, "")    // remove html style group
+        .replace(/<\/?[^>]+>/ig, "")        // remove html tags
 }
