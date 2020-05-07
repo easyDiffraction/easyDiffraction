ee47750400142239350525656192e3eea449fcc8
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2020-05-07 17:31:45 +0200

    Fix cif syntax highlighting

--- App/QmlImports/easyDiffraction/Logic/Helpers.js
+++ App/QmlImports/easyDiffraction/Logic/Helpers.js
@@ -1,10 +1,10 @@
 function highlightCifSyntax(str)
 {
     return str
-        .replace(/\n(_[\w,-]+) /ig, "\n<font color=#2380b5>$1</font> ") // colorize individual keys (with space at the end), e.g., "_setup_wavelength"
-        .replace(/\n(_[\w,-]+)/ig, "\n<font color=#669431>$1</font>")   // colorize keys inside loops (with new line at the end), e.g., "_phase_label"
-        .replace(/loop_/ig, "<font color=#888>loop_</font>")            // colorize "loop_"
         .replace(/data_\w+/ig, "<font color=#b95e39>$&</font>")         // colorize datablock, e.g., "data_Fe3O4"
+        .replace(/loop_/ig, "<font color=#888>loop_</font>")            // colorize "loop_"
+        .replace(/(_[\w,-]+) /ig, "<font color=#2380b5>$1</font> ")     // colorize individual keys (with space at the end), e.g., "_setup_wavelength"
+        .replace(/(_[\w,-]+)\n/ig, "<font color=#669431>$1</font>\n")   // colorize keys inside loops (with new line at the end), e.g., "_phase_label"
         .replace(/(\r\n|\r|\n)/ig, "<br />")                            // change newline and carriage return escape sequences to html format
 }
 
